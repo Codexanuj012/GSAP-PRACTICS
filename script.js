648b0be706b2e9ae3989b4568b515b7fc1033d4a
Anuj Kushawaha <95anuj.ak@gamil.com>
2025-11-29 12:49:45 +0530

    All Submit by Author

---
+++ script.js
@@ -0,0 +1,9 @@
+// rotate and move elements with a class of "box"
+// ("x" is a shortcut for a translateX() transform) over the course of 1 second.
+gsap.to(".box", { rotation: 180, x: 1800, duration: 10 ,
+    
+});
+gsap.from(".box1", { rotation: 180, y: 1000, duration: 10 ,
+    
+
+});
